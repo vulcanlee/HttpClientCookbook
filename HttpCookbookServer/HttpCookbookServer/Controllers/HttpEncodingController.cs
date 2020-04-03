db906104f84310aa219693ef6c793a5f28fdcb8c
Vulcan <vulcan.lee@gmail.com>
2020-04-03 23:40:57 +0800

    不同 HttpContext 的資料編碼的相關 API

--- HttpCookbookServer/HttpCookbookServer/Controllers/HttpEncodingController.cs
+++ HttpCookbookServer/HttpCookbookServer/Controllers/HttpEncodingController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HttpCookbookServer.Dtos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,22 +14,66 @@ namespace HttpCookbookServer.Controllers
     {
         // GET: api/HttpEncoding
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<HttpEncodingRequestDto> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new HttpEncodingRequestDto[] {
+                new HttpEncodingRequestDto()
+                {
+                     Name = "王小明", Age=10
+                }, new HttpEncodingRequestDto()
+                {
+                    Name = "張小美",Age=18
+                }
+            };
         }
 
-        // GET: api/HttpEncoding/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        [HttpGet("{name}/{age}")]
+        public string ByRouting(string name, int age)
         {
-            return "value";
+            return $"HttpGet > ByRouting 得到的請求內容： Name={name} , Age={age}"; ;
         }
 
-        // POST: api/HttpEncoding
+        [HttpGet]
+        [Route("ByQueryString")]
+        public string ByQueryString( string name, int age)
+        {
+            return $"HttpGet > ByQueryString 得到的請求內容： Name={name} , Age={age}"; ;
+        }
+
+        [HttpGet]
+        [Route("ByForm")]
+        public string ByForm([FromForm] string name, [FromForm]int age)
+        {
+            return $"HttpGet > ByForm 得到的請求內容： Name={name} , Age={age}"; ;
+        }
+
+        [HttpGet]
+        [Route("ByFromFormComplex")]
+        public string ByFromFormComplex([FromForm] HttpEncodingRequestDto httpEncodingRequestDto)
+        {
+            return $"HttpGet > ByFromFormComplex 得到的請求內容： Name={httpEncodingRequestDto.Name} , Age={httpEncodingRequestDto.Age}";
+        }
+
+        [HttpGet]
+        [Route("ByFromBody")]
+        public string ByFromBody([FromBody] HttpEncodingRequestDto httpEncodingRequestDto)
+        {
+            return $"HttpGet > ByFromBody 得到的請求內容： Name={httpEncodingRequestDto.Name} , Age={httpEncodingRequestDto.Age}";
+        }
+
+
         [HttpPost]
-        public void Post([FromBody] string value)
+        [Route("ByPostJson")]
+        public string ByPostJson(HttpEncodingRequestDto httpEncodingRequestDto)
         {
+            return $"HttpPost > ByPostJson 得到的請求內容： Name={httpEncodingRequestDto.Name} , Age={httpEncodingRequestDto.Age}";
+        }
+
+        [HttpPost]
+        [Route("ByPostFromForm")]
+        public string ByPostFromForm([FromForm] HttpEncodingRequestDto httpEncodingRequestDto)
+        {
+            return $"HttpPost > ByPostFromForm 得到的請求內容： Name={httpEncodingRequestDto.Name} , Age={httpEncodingRequestDto.Age}";
         }
 
         // PUT: api/HttpEncoding/5
--- HttpCookbookServer/HttpCookbookServer/Startup.cs
+++ HttpCookbookServer/HttpCookbookServer/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace HttpCookbookServer
 {
@@ -26,6 +27,13 @@ namespace HttpCookbookServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            // https://docs.microsoft.com/zh-tw/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-3.1&tabs=visual-studio
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HttpClient Cookbook API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +46,16 @@ namespace HttpCookbookServer
 
             app.UseHttpsRedirection();
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            
             app.UseRouting();
 
             app.UseAuthorization();
