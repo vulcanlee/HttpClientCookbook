57d4e61b14005e3aaf77ce3b10871f35679cf20d
Vulcan <vulcan.lee@gmail.com>
2020-04-04 21:50:24 +0800

    加入使用說明與註解

--- HttpCookbookServer/HttpCookbookServer/Controllers/HttpEncodingController.cs
+++ HttpCookbookServer/HttpCookbookServer/Controllers/HttpEncodingController.cs
@@ -27,19 +27,33 @@ namespace HttpCookbookServer.Controllers
             };
         }
 
+        // https://localhost:5001/api/HttpEncoding/MyName/12
         [HttpGet("{name}/{age}")]
         public string ByRouting(string name, int age)
         {
             return $"HttpGet > ByRouting 得到的請求內容： Name={name} , Age={age}"; ;
         }
 
+        // https://localhost:5001/api/HttpEncoding/MyName/?age=12
+        [HttpGet("{name}")]
+        [Route("ByRoutingAndQueryString")]
+        public string ByRoutingAndQueryString(string name, int age)
+        {
+            return $"HttpGet > ByRoutingAndQueryString 得到的請求內容： Name={name} , Age={age}"; ;
+        }
+
+        // https://localhost:5001/api/HttpEncoding/ByQueryString/?name=MyName&age=12
         [HttpGet]
         [Route("ByQueryString")]
-        public string ByQueryString( string name, int age)
+        public string ByQueryString(string name, int age)
         {
             return $"HttpGet > ByQueryString 得到的請求內容： Name={name} , Age={age}"; ;
         }
 
+        // https://localhost:5001/api/HttpEncoding/ByForm/?name=MyName&age=12
+        // 或者，使用 Form Data or Form Url 皆會觸發此方法
+        //name:MyName
+        //age:23
         [HttpGet]
         [Route("ByForm")]
         public string ByForm([FromForm] string name, [FromForm]int age)
@@ -47,6 +61,11 @@ namespace HttpCookbookServer.Controllers
             return $"HttpGet > ByForm 得到的請求內容： Name={name} , Age={age}"; ;
         }
 
+        // https://localhost:5001/api/HttpEncoding/ByFromFormComplex
+        // 使用上面的 ByForm 的做法，都可以接收的到
+        // QueryString , Form Data , Form Url 皆會觸發
+        //name:MyName
+        //age:23
         [HttpGet]
         [Route("ByFromFormComplex")]
         public string ByFromFormComplex([FromForm] HttpEncodingRequestDto httpEncodingRequestDto)
@@ -54,6 +73,12 @@ namespace HttpCookbookServer.Controllers
             return $"HttpGet > ByFromFormComplex 得到的請求內容： Name={httpEncodingRequestDto.Name} , Age={httpEncodingRequestDto.Age}";
         }
 
+        // https://localhost:5001/api/HttpEncoding/ByFromBody
+        // 使用 Json 的編碼方式，就可以觸發
+        //{
+        //  "name": "MyName",
+        //  "age": 33
+        //}
         [HttpGet]
         [Route("ByFromBody")]
         public string ByFromBody([FromBody] HttpEncodingRequestDto httpEncodingRequestDto)
@@ -61,7 +86,13 @@ namespace HttpCookbookServer.Controllers
             return $"HttpGet > ByFromBody 得到的請求內容： Name={httpEncodingRequestDto.Name} , Age={httpEncodingRequestDto.Age}";
         }
 
-
+        // https://localhost:5001/api/HttpEncoding/ByPostJson
+        // 記得，方法要改成 POST
+        // 使用 Json 的編碼方式，就可以觸發
+        //{
+        //  "name": "MyName",
+        //  "age": 33
+        //}
         [HttpPost]
         [Route("ByPostJson")]
         public string ByPostJson(HttpEncodingRequestDto httpEncodingRequestDto)
@@ -69,6 +100,11 @@ namespace HttpCookbookServer.Controllers
             return $"HttpPost > ByPostJson 得到的請求內容： Name={httpEncodingRequestDto.Name} , Age={httpEncodingRequestDto.Age}";
         }
 
+        // https://localhost:5001/api/HttpEncoding/ByPostFromForm
+        // 記得，方法要改成 POST
+        // QueryString , Form Data , Form Url 皆會觸發
+        //name:MyName
+        //age:23
         [HttpPost]
         [Route("ByPostFromForm")]
         public string ByPostFromForm([FromForm] HttpEncodingRequestDto httpEncodingRequestDto)
